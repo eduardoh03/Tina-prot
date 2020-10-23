462dbe8a927ecd0b5458d038be46af3caffe1a53
Eduardo Henrique <eduhenriquesrs@gmail.com>
2020-10-22 22:58:16 -0300

    Projeto Criado

---
+++ src/main/java/com/somostina/tina/TinaApplication.java
@@ -0,0 +1,13 @@
+package com.somostina.tina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/somostina/tina/TinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.somostina.tina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
