4da81929f83fbe973e2516150dc820fd86783eb6
Eduardo Henrique <eduhenriquesrs@gmail.com>
2020-10-23 00:27:30 -0300

    Criado a instanciacao

--- src/main/java/com/somostina/tina/TinaApplication.java
+++ src/main/java/com/somostina/tina/TinaApplication.java
@@ -1,13 +1,28 @@
 package com.somostina.tina;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.somostina.tina.domain.Servico;
+import com.somostina.tina.repositories.ServicoRepository;
+
 @SpringBootApplication
-public class TinaApplication {
+public class TinaApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TinaApplication.class, args);
-	}
+		}
+		@Autowired
+		private ServicoRepository servicoRepository;
+		public void run(String... args) throws Exception {
+			Servico serv1 = new Servico(null, "Cabelo");
+			Servico serv2 = new Servico(null, "Maquiagem");
+
+			servicoRepository.saveAll(Arrays.asList(serv1, serv2));
+		}
 
 }
