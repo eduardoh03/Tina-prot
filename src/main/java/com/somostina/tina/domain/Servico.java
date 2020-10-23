d1294a11f879756b4f480308c7beb03cc9835514
Eduardo Henrique <eduhenriquesrs@gmail.com>
2020-10-22 23:43:01 -0300

    Banco de dados H2

--- src/main/java/com/somostina/tina/domain/Servico.java
+++ src/main/java/com/somostina/tina/domain/Servico.java
@@ -2,12 +2,19 @@ package com.somostina.tina.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Servico implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-	private Double preco;
 	
 	public Servico() {
 	}
@@ -34,13 +41,6 @@ public class Servico implements Serializable{
 		this.nome = nome;
 	}
 
-	public Double getPreco() {
-		return preco;
-	}
-
-	public void setPreco(Double preco) {
-		this.preco = preco;
-	}
 
 	@Override
 	public int hashCode() {
