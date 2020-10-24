82962b68203fdb14c570b7386be56212d0a75e7a
Eduardo Henrique <eduhenriquesrs@gmail.com>
2020-10-24 16:59:52 -0300

    CRUD Servicos completo, DataIntegrityException tratado

--- src/main/java/com/somostina/tina/resources/ServicoResource.java
+++ src/main/java/com/somostina/tina/resources/ServicoResource.java
@@ -1,6 +1,7 @@
 package com.somostina.tina.resources;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,13 +28,31 @@ public class ServicoResource {
 		Servico obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-	
-	
+		
 	@PostMapping
 	public ResponseEntity<Void> insert(@RequestBody Servico obj){
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@RequestBody Servico obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.ok().build();
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id){
+		service.delete(id);
+		return ResponseEntity.ok().build();
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<Servico>> findAll() {
+		List<Servico> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/somostina/tina/services/ServicoService.java
+++ src/main/java/com/somostina/tina/services/ServicoService.java
@@ -1,12 +1,15 @@
 package com.somostina.tina.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.somostina.tina.domain.Servico;
 import com.somostina.tina.repositories.ServicoRepository;
+import com.somostina.tina.services.exceptions.DataIntegrityException;
 import com.somostina.tina.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,12 +21,29 @@ public class ServicoService {
 	public Servico find(Integer id) {
 		Optional<Servico> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				"Objeto não encontrado! Id: " + id + 
-				", Tipo: " + Servico.class.getName()));
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Servico.class.getName()));
+	}
+
+	public Servico insert(Servico obj) {
+		obj.setId(null);// impedir que seja uma atualização
+		return repo.save(obj);
+	}
+
+	public Servico update(Servico obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir um Serviço que possui Procedimentos");
+		}
 	}
 	
-	public Servico insert(Servico obj) {
-		obj.setId(null);//impedir que seja uma atualização
-		return repo.save(obj);
+	public List<Servico> findAll(){
+		return repo.findAll();
 	}
 }
